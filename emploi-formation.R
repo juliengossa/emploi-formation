f8914b43cc989dc610408f449ea3677c14337062
Camille <camille.schumacher9@gmail.com>
2022-05-24 16:24:46 +0200

    Correction du code pour afficher le graphique correctement

--- emploi-formation.R
+++ emploi-formation.R
@@ -1,6 +1,7 @@
 
 library(tidyverse)
 library(ggcpesrthemes)
+
 theme_cpesr_setup(source="INSEE, enquête emploi en continu 2003-2020")
 
 options(dplyr.summarise.inform = FALSE)
@@ -10,11 +11,10 @@ plot_activite <- function(agemin = 15, agemax = 30) {
     filter(Age > agemin, Age < agemax) %>%
     group_by(Annee,Activite) %>%
     summarise(Population = sum(Population)) %>%
-    mutate(Activite = factor(Activite,levels=c("Actif_occupe","Etudiant","Chomeur_ou_inactif"))) %>%
+    mutate(Activite = factor(Activite,labels=c("Actif occupe","Etudiant","Chomeur ou inactif"))) %>%
     ggplot(aes(x=Annee,y=Population,fill=Activite,group=Activite)) +
     geom_area(color="white") 
 }
-
 
 
 
