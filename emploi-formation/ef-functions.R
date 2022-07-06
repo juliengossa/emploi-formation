3a1c37fa10691399eabd7acda11b2f7188afc8f9
juliengossa <julien.gossa@gmail.com>
2022-07-06 17:28:32 +0200

    App shiny : ajout d'un complete

--- emploi-formation/ef-functions.R
+++ emploi-formation/ef-functions.R
@@ -18,6 +18,8 @@ plot_categorie <- function(categorie="Activite", agemin = 15, agemax = 30, annee
     group_by(Annee, Categorie=!!as.name(categorie)) %>%
     summarise(Population = sum(Population, na.rm=TRUE)) %>%
     { if (keep_na) na.omit(.) else . } %>%
+    ungroup() %>%
+    complete(Annee, Categorie, fill = list(Population = 0)) %>%
     ggplot(aes(x=Annee,y=Population,fill=Categorie,color=Categorie, group=Categorie)) +
     geom_area(alpha=0.7, position = position_geom) + 
     scale_y_continuous(labels = label_scale) +
@@ -43,6 +45,8 @@ plot_categorie_slice <- function(categorie="Activite", agemin = 15, agemax = 30,
     group_by(Annee, Age, Categorie=!!as.name(categorie)) %>%
     summarise(Population = sum(Population, na.rm=TRUE)) %>%
     { if (keep_na) na.omit(.) else . } %>%
+    ungroup() %>%
+    complete(Annee, Categorie, fill = list(Population = 0)) %>%
     ggplot(aes(x=Age,y=Population,fill=Categorie, group=Categorie)) +
     geom_col(alpha=0.7, position = position_geom, color="black",width = 1) + 
     facet_grid(.~Annee) +
@@ -70,6 +74,8 @@ plot_categorie_ponctuel <- function(categorie="Activite", agemin = 15, agemax = 
     summarise(Population = sum(Population, na.rm=TRUE)) %>%
     { if (keep_na) na.omit(.) else . } %>%
     { if (fill) group_by(.,Annee) %>% mutate(Population = Population / sum(Population)) else . } %>%
+    ungroup() %>%
+    complete(Annee, Categorie, fill = list(Population = 0)) %>%
     ggplot(aes(x=Categorie,y=Population,fill=Categorie, group=Categorie)) +
     geom_col(alpha=0.7, color="black",width = 1) + 
     facet_grid(.~Annee) +
